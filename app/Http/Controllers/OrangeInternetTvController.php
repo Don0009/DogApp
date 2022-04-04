f6ba3690c21e010c4d1fd233b6bcea446fe39f64
musmangeee <musmangeee@bitbucket.org>
2022-04-05 00:13:16 +0500

    package updated and path added

--- app/Http/Controllers/OrangeInternetTvController.php
+++ app/Http/Controllers/OrangeInternetTvController.php
@@ -185,8 +185,8 @@ class OrangeInternetTvController extends Controller
         $pdf = new Pdf(public_path('notfill.pdf'), [
 //            'command' => '/some/other/path/to/pdftk',
             // or on most Windows systems:
-            'command' => 'C:\Program Files (x86)\PDFtk Server\bin\pdftk.exe',
-            'useExec' => true,  // May help on Windows systems if execution fails
+            'command' => '\snap\bin\pdftk',
+//            'useExec' => true,  // May help on Windows systems if execution fails
         ]);
 
         $data = $orange = OrangeInternetTv::create($data);
