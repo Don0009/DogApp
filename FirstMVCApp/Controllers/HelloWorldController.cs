14a3c912c92b0b72688752de09113f6ce34a4257
Saad Anwar <zaadgalaxya10@gmail.com>
2022-06-16 14:54:28 +0500

    This is the very first commit in the Dog App Respository

---
+++ FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstMVCApp.Models;
+//using System.Collections;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+        public IActionResult Index()
+        {
+           
+
+             return View(dogs);
+
+
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        // for the form
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            //return View("Index");
+            dogs.Add(dogViewModel); 
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        public string Hello()
+        {
+            return "Who is theer";
+        }
+    }
+}
---
+++ FirstMVCApp/Models/DogViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FirstMVCApp.Models
+{
+    public class DogViewModel
+    {
+        public string  Name { get; set; } 
+        public string Age { get; set; }
+    }
+}
